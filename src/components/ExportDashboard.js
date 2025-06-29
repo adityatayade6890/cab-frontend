92b2d18b188c4335e8a75b9c390ac64daed496a0
Aditya Tayade <adityatayade549@gmail.com>
2025-06-29 23:41:45 +0530

    Api url corrected

--- src/components/ExportDashboard.js
+++ src/components/ExportDashboard.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const ExportDashboard = () => {
   const [fromDate, setFromDate] = useState('');
