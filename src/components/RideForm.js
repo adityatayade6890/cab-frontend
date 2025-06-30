43efb66cfd40497a17ae554a0221a363867df7a8
adityatayade6890 <adityatayade549@gmail.com>
2025-06-30 23:45:35 +0530

    Fixed doc.save

--- src/components/RideForm.js
+++ src/components/RideForm.js
@@ -163,7 +163,7 @@ const GenerateBillForm = () => {
     doc.text('Authorized Signatory', 200, finalY + 48, { align: 'right' });
 
     // ====== SAVE PDF ======
-    doc.save(`${billData.used_by}_${billData.model}_${billData.invoiceDate}.pdf`);
+    doc.save(`BILL_${billData.used_by}_${billData.model}_${billData.invoiceDate}.pdf`);
   };
 
 
